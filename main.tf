46242b42103b57843f9c8c3418ca333ecd218bab
Jeka4el <ehodwork@gmail.com>
2022-06-13 18:55:05 +0300

    Created security group and connected to my VPC

--- main.tf
+++ main.tf
@@ -61,3 +61,37 @@ resource "aws_route_table_association" "a-front-net" {
   route_table_id = aws_route_table.global-RT.id
 }
 
+# Created security_group ssh and web
+
+resource "aws_security_group" "global-sg" {
+  name        = "ssh-web"
+  description = "Allow 22 and 80 ports traffic"
+  vpc_id      = aws_vpc.global-vpc.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "WEB from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-web-sg"
+  }
+}
