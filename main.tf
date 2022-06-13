502457aac1245855e5ebfc69dfed44be084d2c4e
Jeka4el <ehodwork@gmail.com>
2022-06-13 16:56:25 +0300

    added VPS and subnets

--- main.tf
+++ main.tf
@@ -1,8 +1,34 @@
 # Логинюсь в AWS , кдючи нужно спрятать в переменные.  
 provider "aws" {
-  region     = "eu-central-1"
+  region     = "eu-west-3"
   access_key = "AKIXXXXXXXXXXXXXXX"
   secret_key = "3AkQwFI8w6XsICuxxxxxxxxxxxxxx"
+}
+
+#Создал глобальную VPC , в ней будет две посети Public, Private
+resource "aws_vpc" "global-vpc" {
+     cidr_block = "10.0.0.0/16"
+      tags = {
+        Name = "global-NET"
+  }
+}
+# Привязываю подсеть. 
+resource "aws_subnet" "front-end-net" {
+  vpc_id     = aws_vpc.global-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public-net"
+  }
+}
+# Привязываю подсеть. 
+resource "aws_subnet" "back-end-net" {
+  vpc_id     = aws_vpc.global-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Private-net"
+  }
 }
 
 
