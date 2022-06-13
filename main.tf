00dcb226c5843d3886a53260a9d0f3b0c10f5846
Jeka4el <ehodwork@gmail.com>
2022-06-13 15:17:50 +0300

    Added login AWS

---
+++ main.tf
@@ -0,0 +1,8 @@
+# Логинюсь в AWS , кдючи нужно спрятать в переменные.  
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = "AKIXXXXXXXXXXXXXXX"
+  secret_key = "3AkQwFI8w6XsICuxxxxxxxxxxxxxx"
+}
+
+
